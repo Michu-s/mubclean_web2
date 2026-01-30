974e1fc672db154fae0607493d6516dfe547ee8e
Michu-s <brandon121005@gmail.com>
2026-01-30 10:37:45 -0800

    actualizaciones

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3753960803"
+    serviceWorkerVersion: "3919686988"
   }
 });
